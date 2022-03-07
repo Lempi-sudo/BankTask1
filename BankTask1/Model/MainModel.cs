dd24c9af30739c7f8efbb7bd00710de149c47a5a
Lempi-sudo <andreii.lapschow1999@mail.ru>
2022-03-07 20:41:39 +0400

    Раскидал по папкам

---
+++ BankTask1/Model/MainModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BankTask1
+{
+    public class DataForSave
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int PriorityClass { get; set; }
+
+    }
+
+    public class ParserStringToDataForSave
+    {
+
+        public static DataForSave parse(string text)
+        {
+            Regex regex = new Regex(@"[А-Яа-яёЁ]+");
+            MatchCollection matches = regex.Matches(text);
+            List<string> nameInit = new List<string>();
+
+            string name = "";
+
+            foreach (Match m in matches)
+            {
+                name += m.Value + " ";
+            }
+
+            Regex regexNum = new Regex(@"\d+");
+            MatchCollection matchesNum = regexNum.Matches(text);
+
+            int id = int.Parse(matchesNum[0].Value);
+            int priority = int.Parse(matchesNum[1].Value);
+
+            DataForSave res = new DataForSave { Name = name, Id = id, PriorityClass = priority };
+            return res;
+        }
+
+    }
+
+
+
+}
---
+++ BankTask1/ViewModel/MainWindowModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Windows;
+using System.Text.RegularExpressions;
+using System.IO;
+using CsvHelper.Configuration;
+using System.Globalization;
+using CsvHelper;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+
+namespace BankTask1
+{
+    class MainWindowModel : INotifyPropertyChanged
+    {
+        private DataForSave _data;
+        
+        public MainWindowModel()
+        {
+            Save = new RelayCommand(_ => save());
+        }
+       
+        private string _text = "Задача , процесс 2 13 ";
+        
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                _text = value;
+                OnPropertyChanged(nameof(Text));
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ICommand Save { get;}
+        
+
+        private void save()
+        {
+            _data = ParserStringToDataForSave.parse(_text);
+
+            string path = @"C:\Users\" + Environment.UserName + @"\Documents\Документ Task1.csv";
+
+            using (var writer = new StreamWriter(path, false, Encoding.GetEncoding("windows-1251")))
+            {
+                var csvConfig = new CsvConfiguration(CultureInfo.GetCultureInfo("ru-RU"))
+                {
+                    HasHeaderRecord = false,
+                    Delimiter = ";"
+                };
+
+                using (var csv = new CsvWriter(writer, csvConfig))
+                {
+                    csv.WriteRecord(_data);
+                }
+
+            
+                Process.Start(path);
+
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+    
+
+   
+
+
+     
+
